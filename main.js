656f79f54627614734e87e6fccd2b9b3d60f0bb8
William Kimball <wkkimball043@gmail.com>
2018-05-23 14:41:48 -0500

    wrote function that creates cards and adds them into the DOM, and can change color of text and background. Need to make it work with multiple cards.

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+let cardNum = 0;
+
 document.querySelector("#createBtn").addEventListener("click", function(){
-    document.querySelector("#cardSpot").innerHTML =`<div class='card'><div class='card-header'>Night</div><div class='card-main'><i class="material-icons">hot_tub</i><div class='main-description'>Hot Tub</div>`
+    cardNum++;
+    document.querySelector("#cardSpot").innerHTML += `<div class="card"><input type="color" name="color1" class="backColor"><input type="color" name="color2" class="fontColor"><button class="deleteBtn">Delete</button><div class='card' id='${cardNum}'>${document.querySelector("#createInput").value}</div></div>`;
+ 
+    let changeBackColor = () => {
+            document.querySelector(".card").style.backgroundColor = event.target.value;
+    }
+    let changeFontColor = () => {
+        document.querySelector(".card").style.color = event.target.value;
+}
+
+    document.querySelector(".backColor").addEventListener("change", changeBackColor);
+    document.querySelector(".fontColor").addEventListener("change", changeFontColor);
+
+    document.querySelectorAll(".deleteBtn").forEach(function(element){
+        console.log(element)
+    }
+);
 })
+// .addEventListener("click", function (){
+//     document.querySelector(`#cardSpot`).innerHTML = "";
+// });

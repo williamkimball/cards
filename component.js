ec03e128522f91f806a639881de5615a1906c26f
William Kimball <wkkimball043@gmail.com>
2018-05-23 15:54:12 -0500

    further componentized component.js, changed positions of functions, moved event listeners to element creation section

--- component.js
+++ component.js
@@ -3,6 +3,13 @@ let cardNum = 0;
 document.querySelector("#createBtn").addEventListener("click", function(){
     cardNum++;
 
+    let changeBackColor = () => {
+        event.target.parentNode.style.backgroundColor = event.target.value;
+        }
+    let changeFontColor = () => {
+        event.target.parentNode.style.color = event.target.value;
+    }
+
     let mainDiv = document.createElement("div");
     mainDiv.className = "card";
     
@@ -10,44 +17,28 @@ document.querySelector("#createBtn").addEventListener("click", function(){
     input1.className = "backColor";
     input1.type = "color";
     input1.name = "color1";
+    input1.addEventListener("change", changeBackColor);
 
     let input2 = document.createElement("input");
     input2.className = "fontColor";
     input2.type = "color";
     input2.name = "color2";
+    input2.addEventListener("change", changeFontColor);
 
     let deleteButton = document.createElement("button");
     deleteButton.className = "deleteBtn";
-    deleteButton.textContent = "Delete Card"
+    deleteButton.textContent = "Delete Card";
+    deleteButton.addEventListener("click", function (){
+        event.target.parentNode.remove();
+    });
 
     let cardDiv = document.createElement('div');
     cardDiv.id = `${cardNum}`;
-    cardDiv.textContent = `${document.querySelector("#createInput").value}`
+    cardDiv.textContent = `${document.querySelector("#createInput").value}`;
 
     document.querySelector("#cardSpot").appendChild(mainDiv).appendChild(input1);
     mainDiv.appendChild(input2);
     mainDiv.appendChild(deleteButton);
     mainDiv.appendChild(cardDiv);
 
-    let changeBackColor = () => {
-        event.target.parentNode.style.backgroundColor = event.target.value;
-        }
-    let changeFontColor = () => {
-        event.target.parentNode.style.color = event.target.value;
-        }
-
-    document.querySelectorAll(".backColor").forEach(function(element){
-        element.addEventListener("change", changeBackColor);
-        });
-             
-    document.querySelectorAll(".fontColor").forEach(function(element){
-        element.addEventListener("change", changeFontColor);
-        });
-    
-    document.querySelectorAll(".deleteBtn").forEach(function(element){
-        element.addEventListener("click", function (){
-                event.target.parentNode.remove();
-             });
-        }
-    );
 });

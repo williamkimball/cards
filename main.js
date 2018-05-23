063a71078fe9b4a36508dc1389934737d75368bb
William Kimball <wkkimball043@gmail.com>
2018-05-23 11:57:30 -0500

    added styles.css and changed the append child to inner html

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 document.querySelector("#createBtn").addEventListener("click", function(){
-    document.querySelector("#cardSpot").appendChild("<div class='card'><div>")
+    document.querySelector("#cardSpot").innerHTML =`<div class='card'><div class='card-header'>Night</div><div class='card-main'><i class="material-icons">hot_tub</i><div class='main-description'>Hot Tub</div>`
 })

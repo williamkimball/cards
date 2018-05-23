5c534acdd58cfa34083e73006865cccbe299d7b7
William Kimball <wkkimball043@gmail.com>
2018-05-23 14:58:28 -0500

    made color change to work with all cards

--- main.js
+++ main.js
@@ -5,24 +5,22 @@ document.querySelector("#createBtn").addEventListener("click", function(){
     document.querySelector("#cardSpot").innerHTML += `<div class="card"><input type="color" name="color1" class="backColor"><input type="color" name="color2" class="fontColor"><button class="deleteBtn">Delete</button><div class='card' id='${cardNum}'>${document.querySelector("#createInput").value}</div></div>`;
  
     let changeBackColor = () => {
-            document.querySelector(".card").style.backgroundColor = event.target.value;
+        event.target.parentNode.style.backgroundColor = event.target.value;
     }
     let changeFontColor = () => {
-        document.querySelector(".card").style.color = event.target.value;
+        event.target.parentNode.style.color = event.target.value;
 }
 
     document.querySelectorAll(".backColor").forEach(function(element){
         element.addEventListener("change", changeBackColor);
     }
-    )
+    );
              
     document.querySelectorAll(".fontColor").forEach(function(element){
         element.addEventListener("change", changeFontColor)
     }
-             );
+     );
     
-
-
     document.querySelectorAll(".deleteBtn").forEach(function(element){
         element.addEventListener("click", function (){
                 event.target.parentNode.innerHTML = "";
